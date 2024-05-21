7aeb022f5261f950e0d53777ace7a0e31149c677
src_resources <src.res.211@gmail.com>
2024-05-21 17:36:08 +0800

    Add section 3.1

---
+++ crates/3.model_loading/1.model_loading/src/main.rs
@@ -0,0 +1,182 @@
+extern crate nalgebra_glm as glm;
+
+use std::sync::Mutex;
+use glfw::{Action, Context, CursorMode, Key, OpenGlProfileHint, Window, WindowHint};
+use learnopengl_shared::{filesystem, util};
+use learnopengl_shared::shader::Shader;
+use lazy_static::lazy_static;
+use learnopengl_shared::camera::{Camera, Movement};
+use learnopengl_shared::model::Model;
+
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
+
+// camera
+lazy_static! {
+    static ref CAMERA: Mutex<Camera> = Mutex::new(Camera::new_position(glm::vec3(0.0, 0.0, 3.0)));
+}
+static mut LAST_X: f32 = 800.0 / 2.0;
+static mut LAST_Y: f32 = 600.0 / 2.0;
+static mut FIRST_MOUSE: bool = false;
+
+// timing
+static mut DELTA_TIME: f32 = 0.0;
+static mut LAST_FRAME: f32 = 0.0;
+
+fn main() {
+    // glfw: initialize and configure
+    // ------------------------------
+    let mut glfw = glfw::init(glfw::fail_on_errors)
+        .expect("Failed to initialise GLFW.");
+
+    glfw.window_hint(WindowHint::ContextVersionMajor(3));
+    glfw.window_hint(WindowHint::ContextVersionMinor(3));
+    glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
+    glfw.window_hint(WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    // --------------------
+    let (mut window, _) = glfw.create_window(
+        SCR_WIDTH, SCR_HEIGHT,
+        "LearnOpenGL", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+    window.set_framebuffer_size_callback(framebuffer_size_callback);
+    window.set_cursor_pos_callback(mouse_callback);
+    window.set_scroll_callback(scroll_callback);
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    // tell GLFW to capture our mouse
+    window.set_cursor_mode(CursorMode::Disabled);
+
+    // load all OpenGL function pointers
+    // ---------------------------------
+    gl::load_with(|s| window.get_proc_address(s) as *const _);
+
+    unsafe {
+        // configure global opengl state
+        // -----------------------------
+        gl::Enable(gl::DEPTH_TEST);
+
+        // build and compile shaders
+        // -------------------------
+        let our_shader = Shader::new("1.model_loading.vs".to_string(), "1.model_loading.fs".to_string(), None);
+
+        // load models
+        // -----------
+        let our_model = Model::new_without_gamma(filesystem::get_path("resources/objects/backpack/backpack.obj".to_string()));
+
+        // draw in wireframe
+        gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+
+        // render loop
+        // -----------
+        while !window.should_close() {
+            // per-frame time logic
+            // --------------------
+            let current_frame = glfw.get_time() as f32;
+            DELTA_TIME = current_frame - LAST_FRAME;
+            LAST_FRAME = current_frame;
+
+            // input
+            // -----
+            process_input(&mut window);
+
+            // render
+            // ------
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
+            // don't forget to enable shader before setting uniforms
+            our_shader.use_shader();
+
+            // view/projection transformations
+            let projection = glm::perspective(CAMERA.lock().unwrap().zoom().to_radians(), (SCR_WIDTH as f32) / (SCR_HEIGHT as f32), 0.1, 100.0);
+            let view = CAMERA.lock().unwrap().get_view_matrix();
+            our_shader.set_mat4("projection".to_string(), &projection);
+            our_shader.set_mat4("view".to_string(), &view);
+
+            // render the loaded model
+            let mut model = util::glm::diag_mat4(1.0);
+            model = glm::translate(&model, &glm::vec3(0.0, 0.0, 0.0)); // translate it down so it's at the center of the scene
+            model = glm::scale(&model, &glm::vec3(1.0, 1.0, 1.0)); // it's a bit too big for our scene, so scale it down
+            our_shader.set_mat4("model".to_string(), &model);
+            our_model.draw(&our_shader);
+
+            // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+            // -------------------------------------------------------------------------------
+            window.swap_buffers();
+            glfw.poll_events();
+        }
+    }
+}
+
+fn process_input(window: &mut Window) {
+    if window.get_key(Key::Escape) == Action::Press {
+        window.set_should_close(true)
+    }
+
+    if window.get_key(Key::W) == Action::Press {
+        unsafe {
+            CAMERA.lock().unwrap().process_keyboard(Movement::FORWARD, DELTA_TIME);
+        }
+    }
+    if window.get_key(Key::S) == Action::Press {
+        unsafe {
+            CAMERA.lock().unwrap().process_keyboard(Movement::BACKWARD, DELTA_TIME);
+        }
+    }
+    if window.get_key(Key::A) == Action::Press {
+        unsafe {
+            CAMERA.lock().unwrap().process_keyboard(Movement::LEFT, DELTA_TIME);
+        }
+    }
+    if window.get_key(Key::D) == Action::Press {
+        unsafe {
+            CAMERA.lock().unwrap().process_keyboard(Movement::RIGHT, DELTA_TIME);
+        }
+    }
+}
+
+fn framebuffer_size_callback(
+    _: &mut Window,
+    width: i32,
+    height: i32
+) {
+    unsafe {
+        gl::Viewport(0, 0, width, height);
+    }
+}
+
+fn mouse_callback(
+    _: &mut Window,
+    x_pos_in: f64,
+    y_pos_in: f64
+) {
+    let x_pos = x_pos_in as f32;
+    let y_pos = y_pos_in as f32;
+
+    unsafe {
+        if FIRST_MOUSE {
+            LAST_X = x_pos;
+            LAST_Y = y_pos;
+            FIRST_MOUSE = false;
+        }
+
+        let x_offset = x_pos - LAST_X;
+        let y_offset = LAST_Y - y_pos; // reversed since y-coordinates go from bottom to top
+        LAST_X = x_pos;
+        LAST_Y = y_pos;
+
+        CAMERA.lock().unwrap().process_mouse_movement(x_offset, y_offset);
+    }
+}
+
+fn scroll_callback(
+    _: &mut Window,
+    _x_offset: f64,
+    y_offset: f64
+) {
+    CAMERA.lock().unwrap().process_mouse_scroll(y_offset as f32);
+}
