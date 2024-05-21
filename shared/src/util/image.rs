e4a84af1235533b4f8d2c2f996db8cfc670a334f
src_resources <src.res.211@gmail.com>
2024-05-21 20:13:11 +0800

    Add section 4.3.1

--- shared/src/util/image.rs
+++ shared/src/util/image.rs
@@ -11,3 +11,13 @@ pub fn load_image_data_rgba(path: String) -> Result<RgbaImage, Box<dyn Error>> {
     let img = ImageReader::open(path)?.with_guessed_format()?.decode()?.flipv();
     Ok(img.to_rgba8())
 }
+
+pub fn load_image_data_rgb_without_flip(path: String) -> Result<RgbImage, Box<dyn Error>> {
+    let img = ImageReader::open(path)?.with_guessed_format()?.decode()?;
+    Ok(img.to_rgb8())
+}
+
+pub fn load_image_data_rgba_without_flip(path: String) -> Result<RgbaImage, Box<dyn Error>> {
+    let img = ImageReader::open(path)?.with_guessed_format()?.decode()?;
+    Ok(img.to_rgba8())
+}
