ed36a7a8f600a9a2532966c01b650c196e6b9d5a
src_resources <src.res.211@gmail.com>
2024-05-22 10:36:50 +0800

    Add section 4.6.2

---
+++ crates/4.advanced_opengl/6.2.cubemaps_environment_mapping/src/main.rs
@@ -0,0 +1,374 @@
+extern crate nalgebra_glm as glm;
+
+use std::{mem, ptr};
+use std::sync::Mutex;
+use gl::types::*;
+use glfw::{Action, Context, CursorMode, Key, OpenGlProfileHint, Window, WindowHint};
+use learnopengl_shared::{filesystem, util};
+use learnopengl_shared::shader_m::Shader;
+use lazy_static::lazy_static;
+use learnopengl_shared::camera::{Camera, Movement};
+
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
+
+// camera
+lazy_static! {
+    static ref CAMERA: Mutex<Camera> = Mutex::new(Camera::new_position(glm::vec3(0.0, 0.0, 3.0)));
+}
+static mut LAST_X: f32 = 800.0 / 2.0;
+static mut LAST_Y: f32 = 600.0 / 2.0;
+static mut FIRST_MOUSE: bool = false;
+
+// timing
+static mut DELTA_TIME: f32 = 0.0;
+static mut LAST_FRAME: f32 = 0.0;
+
+fn main() {
+    // glfw: initialize and configure
+    // ------------------------------
+    let mut glfw = glfw::init(glfw::fail_on_errors)
+        .expect("Failed to initialise GLFW.");
+
+    glfw.window_hint(WindowHint::ContextVersionMajor(3));
+    glfw.window_hint(WindowHint::ContextVersionMinor(3));
+    glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
+    glfw.window_hint(WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    // --------------------
+    let (mut window, _) = glfw.create_window(
+        SCR_WIDTH, SCR_HEIGHT,
+        "LearnOpenGL", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+    window.set_framebuffer_size_callback(framebuffer_size_callback);
+    window.set_cursor_pos_callback(mouse_callback);
+    window.set_scroll_callback(scroll_callback);
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    // tell GLFW to capture our mouse
+    window.set_cursor_mode(CursorMode::Disabled);
+
+    // load all OpenGL function pointers
+    // ---------------------------------
+    gl::load_with(|s| window.get_proc_address(s) as *const _);
+
+    unsafe {
+        // configure global opengl state
+        // -----------------------------
+        gl::Enable(gl::DEPTH_TEST);
+
+        // build and compile shaders
+        // -------------------------
+        let shader = Shader::new("6.2.cubemaps.vs".to_string(), "6.2.cubemaps.fs".to_string());
+        let skybox_shader = Shader::new("6.2.skybox.vs".to_string(), "6.2.skybox.fs".to_string());
+
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        let cube_vertices = [
+            // positions          // normals
+            -0.5f32, -0.5, -0.5,  0.0,  0.0, -1.0,
+            0.5, -0.5, -0.5,  0.0,  0.0, -1.0,
+            0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+            0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+            -0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+            -0.5, -0.5, -0.5,  0.0,  0.0, -1.0,
+
+            -0.5, -0.5,  0.5,  0.0,  0.0, 1.0,
+            0.5, -0.5,  0.5,  0.0,  0.0, 1.0,
+            0.5,  0.5,  0.5,  0.0,  0.0, 1.0,
+            0.5,  0.5,  0.5,  0.0,  0.0, 1.0,
+            -0.5,  0.5,  0.5,  0.0,  0.0, 1.0,
+            -0.5, -0.5,  0.5,  0.0,  0.0, 1.0,
+
+            -0.5,  0.5,  0.5, -1.0,  0.0,  0.0,
+            -0.5,  0.5, -0.5, -1.0,  0.0,  0.0,
+            -0.5, -0.5, -0.5, -1.0,  0.0,  0.0,
+            -0.5, -0.5, -0.5, -1.0,  0.0,  0.0,
+            -0.5, -0.5,  0.5, -1.0,  0.0,  0.0,
+            -0.5,  0.5,  0.5, -1.0,  0.0,  0.0,
+
+            0.5,  0.5,  0.5,  1.0,  0.0,  0.0,
+            0.5,  0.5, -0.5,  1.0,  0.0,  0.0,
+            0.5, -0.5, -0.5,  1.0,  0.0,  0.0,
+            0.5, -0.5, -0.5,  1.0,  0.0,  0.0,
+            0.5, -0.5,  0.5,  1.0,  0.0,  0.0,
+            0.5,  0.5,  0.5,  1.0,  0.0,  0.0,
+
+            -0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+            0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+            0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+            0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+            -0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+            -0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+
+            -0.5,  0.5, -0.5,  0.0,  1.0,  0.0,
+            0.5,  0.5, -0.5,  0.0,  1.0,  0.0,
+            0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+            0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+            -0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+            -0.5,  0.5, -0.5,  0.0,  1.0,  0.0
+        ];
+        let skybox_vertices = [
+            // positions          
+            -1.0f32,  1.0, -1.0,
+            -1.0, -1.0, -1.0,
+            1.0, -1.0, -1.0,
+            1.0, -1.0, -1.0,
+            1.0,  1.0, -1.0,
+            -1.0,  1.0, -1.0,
+
+            -1.0, -1.0,  1.0,
+            -1.0, -1.0, -1.0,
+            -1.0,  1.0, -1.0,
+            -1.0,  1.0, -1.0,
+            -1.0,  1.0,  1.0,
+            -1.0, -1.0,  1.0,
+
+            1.0, -1.0, -1.0,
+            1.0, -1.0,  1.0,
+            1.0,  1.0,  1.0,
+            1.0,  1.0,  1.0,
+            1.0,  1.0, -1.0,
+            1.0, -1.0, -1.0,
+
+            -1.0, -1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            1.0,  1.0,  1.0,
+            1.0,  1.0,  1.0,
+            1.0, -1.0,  1.0,
+            -1.0, -1.0,  1.0,
+
+            -1.0,  1.0, -1.0,
+            1.0,  1.0, -1.0,
+            1.0,  1.0,  1.0,
+            1.0,  1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            -1.0,  1.0, -1.0,
+
+            -1.0, -1.0, -1.0,
+            -1.0, -1.0,  1.0,
+            1.0, -1.0, -1.0,
+            1.0, -1.0, -1.0,
+            -1.0, -1.0,  1.0,
+            1.0, -1.0,  1.0
+        ];
+
+        // cube VAO
+        let (mut cube_vao, mut cube_vbo) = (0u32, 0u32);
+        gl::GenVertexArrays(1, &mut cube_vao);
+        gl::GenBuffers(1, &mut cube_vbo);
+        gl::BindVertexArray(cube_vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, cube_vbo);
+        gl::BufferData(gl::ARRAY_BUFFER, mem::size_of_val(&cube_vertices) as GLsizeiptr, ptr::addr_of!(cube_vertices) as *const _, gl::STATIC_DRAW);
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, (6 * mem::size_of::<f32>()) as GLsizei, ptr::null());
+        gl::EnableVertexAttribArray(1);
+        gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, (6 * mem::size_of::<f32>()) as GLsizei, (3 * mem::size_of::<f32>()) as *const _);
+        // skybox VAO
+        let (mut skybox_vao, mut skybox_vbo) = (0u32, 0u32);
+        gl::GenVertexArrays(1, &mut skybox_vao);
+        gl::GenBuffers(1, &mut skybox_vbo);
+        gl::BindVertexArray(skybox_vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, skybox_vbo);
+        gl::BufferData(gl::ARRAY_BUFFER, mem::size_of_val(&skybox_vertices) as GLsizeiptr, ptr::addr_of!(skybox_vertices) as *const _, gl::STATIC_DRAW);
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, (3 * mem::size_of::<f32>()) as GLsizei, ptr::null());
+
+        // load textures
+        // -------------
+        let faces = vec![
+            filesystem::get_path("resources/textures/skybox/right.jpg".to_string()),
+            filesystem::get_path("resources/textures/skybox/left.jpg".to_string()),
+            filesystem::get_path("resources/textures/skybox/top.jpg".to_string()),
+            filesystem::get_path("resources/textures/skybox/bottom.jpg".to_string()),
+            filesystem::get_path("resources/textures/skybox/front.jpg".to_string()),
+            filesystem::get_path("resources/textures/skybox/back.jpg".to_string())
+        ];
+        let cubemap_texture = load_cubemap(&faces);
+
+        // shader configuration
+        // --------------------
+        shader.use_shader();
+        shader.set_int("skybox".to_string(), 0);
+
+        skybox_shader.use_shader();
+        skybox_shader.set_int("skybox".to_string(), 0);
+
+        // render loop
+        // -----------
+        while !window.should_close() {
+            // per-frame time logic
+            // --------------------
+            let current_frame = glfw.get_time() as f32;
+            DELTA_TIME = current_frame - LAST_FRAME;
+            LAST_FRAME = current_frame;
+
+            // input
+            // -----
+            process_input(&mut window);
+
+            // render
+            // ------
+            gl::ClearColor(0.1, 0.1, 0.1, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
+            // draw scene as normal
+            shader.use_shader();
+            let model = util::glm::diag_mat4(1.0);
+            let view = CAMERA.lock().unwrap().get_view_matrix();
+            let projection = glm::perspective(CAMERA.lock().unwrap().zoom().to_radians(), (SCR_WIDTH as f32) / (SCR_HEIGHT as f32), 0.1, 100.0);
+            shader.set_mat4("model".to_string(), &model);
+            shader.set_mat4("view".to_string(), &view);
+            shader.set_mat4("projection".to_string(), &projection);
+            shader.set_vec3("cameraPos".to_string(), &CAMERA.lock().unwrap().position());
+            // cubes
+            gl::BindVertexArray(cube_vao);
+            gl::ActiveTexture(gl::TEXTURE0);
+            gl::BindTexture(gl::TEXTURE_CUBE_MAP, cubemap_texture);
+            gl::DrawArrays(gl::TRIANGLES, 0, 36);
+            gl::BindVertexArray(0);
+
+            // draw skybox as last
+            gl::DepthFunc(gl::LEQUAL); // change depth function so depth test passes when values are equal to depth buffer's content
+            skybox_shader.use_shader();
+            let view = util::glm::mat4_wrap_mat3(&util::glm::mat3_from_mat4(&CAMERA.lock().unwrap().get_view_matrix())); // remove translation from the view matrix
+            skybox_shader.set_mat4("view".to_string(), &view);
+            skybox_shader.set_mat4("projection".to_string(), &projection);
+            // skybox cube
+            gl::BindVertexArray(skybox_vao);
+            gl::ActiveTexture(gl::TEXTURE0);
+            gl::BindTexture(gl::TEXTURE_CUBE_MAP, cubemap_texture);
+            gl::DrawArrays(gl::TRIANGLES, 0, 36);
+            gl::BindVertexArray(0);
+            gl::DepthFunc(gl::LESS); // set depth function back to default
+
+            // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+            // -------------------------------------------------------------------------------
+            window.swap_buffers();
+            glfw.poll_events();
+        }
+
+        // optional: de-allocate all resources once they've outlived their purpose:
+        // ------------------------------------------------------------------------
+        gl::DeleteVertexArrays(1, &cube_vao);
+        gl::DeleteVertexArrays(1, &skybox_vao);
+        gl::DeleteBuffers(1, &cube_vbo);
+        gl::DeleteBuffers(1, &skybox_vbo);
+    }
+}
+
+fn process_input(window: &mut Window) {
+    if window.get_key(Key::Escape) == Action::Press {
+        window.set_should_close(true)
+    }
+
+    if window.get_key(Key::W) == Action::Press {
+        unsafe {
+            CAMERA.lock().unwrap().process_keyboard(Movement::FORWARD, DELTA_TIME);
+        }
+    }
+    if window.get_key(Key::S) == Action::Press {
+        unsafe {
+            CAMERA.lock().unwrap().process_keyboard(Movement::BACKWARD, DELTA_TIME);
+        }
+    }
+    if window.get_key(Key::A) == Action::Press {
+        unsafe {
+            CAMERA.lock().unwrap().process_keyboard(Movement::LEFT, DELTA_TIME);
+        }
+    }
+    if window.get_key(Key::D) == Action::Press {
+        unsafe {
+            CAMERA.lock().unwrap().process_keyboard(Movement::RIGHT, DELTA_TIME);
+        }
+    }
+}
+
+fn framebuffer_size_callback(
+    _: &mut Window,
+    width: i32,
+    height: i32
+) {
+    unsafe {
+        gl::Viewport(0, 0, width, height);
+    }
+}
+
+fn mouse_callback(
+    _: &mut Window,
+    x_pos_in: f64,
+    y_pos_in: f64
+) {
+    let x_pos = x_pos_in as f32;
+    let y_pos = y_pos_in as f32;
+
+    unsafe {
+        if FIRST_MOUSE {
+            LAST_X = x_pos;
+            LAST_Y = y_pos;
+            FIRST_MOUSE = false;
+        }
+
+        let x_offset = x_pos - LAST_X;
+        let y_offset = LAST_Y - y_pos; // reversed since y-coordinates go from bottom to top
+        LAST_X = x_pos;
+        LAST_Y = y_pos;
+
+        CAMERA.lock().unwrap().process_mouse_movement(x_offset, y_offset);
+    }
+}
+
+fn scroll_callback(
+    _: &mut Window,
+    _x_offset: f64,
+    y_offset: f64
+) {
+    CAMERA.lock().unwrap().process_mouse_scroll(y_offset as f32);
+}
+
+// loads a cubemap texture from 6 individual texture faces
+// order:
+// +X (right)
+// -X (left)
+// +Y (top)
+// -Y (bottom)
+// +Z (front)
+// -Z (back)
+// -------------------------------------------------------
+fn load_cubemap(faces: &Vec<String>) -> u32 {
+    let mut texture_id = 0u32;
+    unsafe {
+        gl::GenTextures(1, &mut texture_id);
+        gl::BindTexture(gl::TEXTURE_CUBE_MAP, texture_id);
+
+        for (i, face) in faces.iter().enumerate() {
+            let img = util::image::load_image_data_rgb_without_flip(face.clone())
+                .expect("Failed to load texture data.");
+            let width = img.width();
+            let height = img.height();
+            let data = img.as_raw();
+
+            gl::TexImage2D(
+                gl::TEXTURE_CUBE_MAP_POSITIVE_X + i as u32,
+                0,
+                gl::RGB as GLint,
+                width as GLint,
+                height as GLint,
+                0,
+                gl::RGB,
+                gl::UNSIGNED_BYTE,
+                data.as_ptr() as *const _
+            );
+        }
+        gl::TexParameteri(gl::TEXTURE_CUBE_MAP, gl::TEXTURE_MIN_FILTER, gl::LINEAR as GLint);
+        gl::TexParameteri(gl::TEXTURE_CUBE_MAP, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
+        gl::TexParameteri(gl::TEXTURE_CUBE_MAP, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as GLint);
+        gl::TexParameteri(gl::TEXTURE_CUBE_MAP, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as GLint);
+        gl::TexParameteri(gl::TEXTURE_CUBE_MAP, gl::TEXTURE_WRAP_R, gl::CLAMP_TO_EDGE as GLint);
+    }
+
+    texture_id
+}
